9f3f21607fcf17c6eef30f8450c261f1a40e1d9f
sachinmsuri <sachinmahantsuri@gmail.com>
2022-03-16 23:25:58 +0000

    updating decentralbank contract and allowing contracts to interact

--- defi-staking-app/migrations/2_deploy_contracts.js
+++ defi-staking-app/migrations/2_deploy_contracts.js
@@ -1,12 +1,23 @@
-const Tether = artifacts.require('Tether')
-const RWD = artifacts.require('RWD')
-const DecentralBank = artifacts.require('DecentralBank')
+const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD');
+const DecentralBank = artifacts.require('DecentralBank');
 
-
-module.exports = async function(deployer) {
+//Deploying the specific currencies --> occurs when the contract is deployed
+module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(Tether);
-    await deployer.deploy(RWD);
-    await deployer.deploy(DecentralBank);
+    const tether = await Tether.deployed()
 
+    await deployer.deploy(RWD);
+    const rwd = await RWD.deployed();
+
+    await deployer.deploy(DecentralBank, rwd.address, tether.address);
+    const decentralBank = await DecentralBank.deployed();
+
+
+    //Transfer all RWD to the Decentral Bank
+    await rwd.transfer(decentralBank.address, '1000000000000000000000000');
+
+    //Give user 100 tether tokens when they come on the application
+    await tether.transfer(accounts[1], '10000000000000000');
 
 };
--- defi-staking-app/src/contracts/DecentralBank.sol
+++ defi-staking-app/src/contracts/DecentralBank.sol
@@ -1,8 +1,18 @@
 pragma solidity ^0.5.0;
 
+import './RWD.sol';
+import './Tether.sol';
+
 //Depositing, Rewarding, Staking our RWD and Tether Tokens
 contract DecentralBank {
     address public owner;
-    string public name = 'DecentralBank';   
+    string public name = 'DecentralBank';
+    Tether public tether;
+    RWD public rwd;
+
+    constructor(RWD _rwd, Tether _tether) public {
+        rwd = _rwd;
+        tether = _tether;
+    }
 
 }
